95c99c634bc9347fc977969f84ee0a2dbda76966
Rochi Albani <rocio.albani@hotmail.com>
2023-04-19 19:12:26 -0300

    preject finished

---
+++ client/src/PlaceImg.jsx
@@ -0,0 +1,14 @@
+
+function PlaceImg({place,index=0, className=null}) {
+    if (!place.photos?.length) {
+        return '';
+    }
+    if(!className) {
+        className = 'object-cover'
+    }
+  return (
+        <img className={className} src={'http://localhost:4000/uploads/'+place.photos[index]} alt="first photo" />
+  )
+}
+
+export default PlaceImg
